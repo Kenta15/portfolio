5d42d49df01b2568ae6d0f04c6c5cfc4b98fd834
Kenta Tanaka <tkenta2929@gmail.com>
2022-07-09 18:40:30 -0700

    flow-field

--- src/js/Experience/SkillsWorld/SkillsWorld.js
+++ src/js/Experience/SkillsWorld/SkillsWorld.js
@@ -1,6 +1,5 @@
 import Texts from './Texts.js'
 import RayCaster from './RayCater.js'
-import Background from './Background.js'
 import Portal from './Portal.js'
 
 export default class SkillsWorld{
@@ -8,7 +7,6 @@ export default class SkillsWorld{
     constructor(){
 
         // this.texts = new Texts()
-        // this.background = new Background()
         this.portal = new Portal()
 
     }
