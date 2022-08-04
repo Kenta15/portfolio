1ed61b161e89462f398e8d5d27f7920ad5b01303
Kenta Tanaka <tkenta2929@gmail.com>
2022-08-04 01:25:06 -0700

    changed linkedIn link

--- src/js/Experience/ContactWorld/Raycaster.js
+++ src/js/Experience/ContactWorld/Raycaster.js
@@ -63,7 +63,7 @@ export default class Raycaster{
                 if(this.clickedText.name == 'text1')
                     window.setTimeout(() => {window.open('https://github.com/Kenta15')}, 200)
                 else if(this.clickedText.name == 'text2')
-                    window.setTimeout(() => {window.open('https://www.linkedin.com/in/kenta-tanaka-0a11a0223/')}, 200)
+                    window.setTimeout(() => {window.open('https://www.linkedin.com/in/kenta05/')}, 200)
                 else if(this.clickedText.name == 'text3')
                     window.setTimeout(() => {window.open('mailto:tkenta2929@gmail.com?')}, 200)
             }
